4eab0d18ed1f9e6b96a402f93a72f7cc7acf76ea
Hiren Dhadhal <hirenhdhadhal@gmail.com>
2020-09-13 01:24:55 +0530

    server updated

---
+++ splitz/fab_four/Backend/Mongoose-model/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  friends: {
+    type: Array,
+  },
+  transaction: {
+   type: Array,
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
