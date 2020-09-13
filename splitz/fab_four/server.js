66ff46eddc3d5e9e38d51b8ee98ecb6f2f6206a2
MadMaxINDIAN <56634706+MadMaxINDIAN@users.noreply.github.com>
2020-09-13 12:56:55 +0530

    server commented

--- splitz/fab_four/server.js
+++ splitz/fab_four/server.js
@@ -1,39 +1,62 @@
-const mongoose = require("mongoose");
-const express = require("express");
-const bodyParser = require("body-parser");
+// const mongoose = require("mongoose");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+
+// // Connecting mongoose
+// mongoose.connect("mongodb+srv://admin:fab_four@cluster0.rhxth.gcp.mongodb.net/fab_four",{ useNewUrlParser: true,useUnifiedTopology: true },() => console.log("MongoDB connected"))
+// mongoose.set('useFindAndModify', false);
+
+// // Router files
+// const dashboard = require("./Backend/router/dashboard");
+// const register = require("./Backend/router/register");
+// const login = require("./Backend/router/login");
+
+// // import User model
+// const User = require("./Backend/Mongoose-model/User")
+
+// // Initialising app
+// const app = express();
+
+// app.set('view engine', 'ejs');
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(express.static("public"));
 
 
+// // Parsing JSON
+// app.use(express.json());
 
-// Connecting mongoose
-mongoose.connect("mongodb+srv://admin:fab_four@cluster0.rhxth.gcp.mongodb.net/fab_four" + database,{ useNewUrlParser: true,useUnifiedTopology: true },() => console.log("MongoDB connected"))
-mongoose.set('useFindAndModify', false);
+// // Initialising router
+// app.use("/api/dashboard/",dashboard);
+// app.use("/api/register/",register)
+// app.use("/api/login/",login)
 
-// Router files
-const dashboard = require("./Backend/router/dashboard");
-const register = require("./Backend/router/register");
-const login = require("./Backend/router/login");
+// app.get("/",(req,res) => {
+//     User.find({} , (err, users) => {
+//         if (err){console.log(err)}
+//     else{
+//         if (users.length == 0){
+//           Item.insertMany(defaultItems,function(err){
+//           if (err) {
+//             console.log(err);
+//           } else {
+//             console.log("Successfully added default items ...");
+//           }
+//         });
+//         res.redirect("/");
+//       }
+//         else {
+//         res.render("list", {listTitle: "Today", newListItems: foundItems});
+//         }
+//       }
+//     })
+//     console.log()
+//     res.send("hello")
+// });
 
-// Initialising app
-const app = express();
+// // Initialisin port
+// port = process.env.PORT || 5000;
 
-// Setting bodyParser
-app.use(bodyParser.urlencoded({extended : true}))
-
-// Parsing JSON
-app.use(express.json());
-
-// Initialising router
-app.use("/api/dashboard/",dashboard);
-app.use("/api/register/",register)
-app.use("/api/login/",login)
-
-app.get("/",(req,res) => {
-    res.send("hello")
-});
-
-// Initialisin port
-port = process.env.PORT || 5000;
-
-app.listen(port, (req,res) => {
-    console.log("Server is running on port " + port);
-});
+// app.listen(port, (req,res) => {
+//     console.log("Server is running on port " + port);
+// });
